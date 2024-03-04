75cc5ba43405df39b0378163e7d668eccfa1c2cd
Charles <charles.chrismann@free.fr>
2024-03-04 17:31:44 +0100

    Initial commit

---
+++ apps/gsh-front-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run gsh-front:serve',
+        production: 'nx run gsh-front:preview',
+      },
+      ciWebServerCommand: 'nx run gsh-front:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
