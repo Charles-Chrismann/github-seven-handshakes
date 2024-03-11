5de3bfb5346f37c1dc614dbe9d3a643822972ba7
Charles <charles.chrismann@free.fr>
2024-03-11 09:32:50 +0100

    ADD redis

--- apps/gsh-back/src/main.ts
+++ apps/gsh-back/src/main.ts
@@ -4,6 +4,66 @@ import * as path from 'path';
 import cors from 'cors'
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { SchemaFieldTypes, createClient } from 'redis';
+
+let client
+// getUser("Charles-Chrismann");
+(async () => {
+  client = await createClient()
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+  try {
+    // console.log(await client.json.get('users:test'))
+    await client.ft.create('idx:users', {
+      '$.redis-updatedAt': {
+        type: SchemaFieldTypes.NUMERIC
+      },
+      '$.id': {
+        type: SchemaFieldTypes.TEXT
+      },
+      '$.login': {
+        type: SchemaFieldTypes.TEXT,
+      },
+      '$.followers': {
+        type: SchemaFieldTypes.TEXT,
+      },
+      '$.followersCount': {
+        type: SchemaFieldTypes.NUMERIC,
+      },
+      '$.followings': {
+        type: SchemaFieldTypes.TEXT,
+      },
+      '$.followingsCount': {
+        type: SchemaFieldTypes.NUMERIC,
+      },
+    }, {
+        ON: 'JSON',
+        PREFIX: 'user:'
+    });
+
+    // const user = await client.json.set('users:test', '$', {
+    //   'redis-updatedAt': 1710085672544,
+    //   id: 'g-and-querying-json-documents',
+    //   login: 'the-test',
+    //   followers: [
+    //     'aaa', 'bbb', 'ccc'
+    //   ],
+    //   followersCount: 3,
+    //   followings: [],
+    //   followingsCount: 0
+    // })
+    // console.log(user)
+  } catch (e) {
+    if (e.message === 'Index already exists') {
+        console.log('Index exists already, skipped creation.');
+    } else {
+        // Something went wrong, perhaps RediSearch isn't installed...
+        console.error(e);
+        process.exit(1);
+    }
+  }
+})()
+
 
 async function getFollowings(userLogin: string): Promise<string[]> {
   try {
@@ -32,6 +92,128 @@ async function getFollowings(userLogin: string): Promise<string[]> {
       console.error(e.message)
     } else console.error(e)
     return []
+  }
+}
+
+async function getUser(userLogin: string): Promise<Record<string, any>> {
+  try {
+    requestCount++
+    const data = await (await fetch('https://api.github.com/graphql', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + process.env["gh-token"]
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        query: `{
+          user(login: "${userLogin}") {
+            id
+            login 
+            following(first: 100) {
+              nodes {
+                id
+                login
+              }
+              totalCount
+              pageInfo {
+                endCursor
+                hasNextPage
+                hasPreviousPage
+                startCursor
+              }
+            }
+          }
+          organization(login: "${userLogin}") {
+            id
+            login 
+          }
+        }`
+      })
+    })).json()
+    if(!data.data.user) {
+      return ({
+        'redis-updatedAt': Date.now(),
+        id: data.data.organization.id,
+        login: data.data.organization.login,
+        followings: [],
+        followingsCount: 0,
+      })
+    }
+    return ({
+      'redis-updatedAt': Date.now(),
+      id: data.data.user.id,
+      login: data.data.user.login,
+      followings: data.data.user.following.nodes.map(follwoing => follwoing.id),
+      followingsCount: data.data.user.following.totalCount,
+    })
+    // return data.data.user ? data.data.user.following.nodes.map(user => user.login) : []
+  } catch (e) {
+    if(e instanceof TypeError) {
+      console.error(e.message)
+    } else console.error(e)
+    return null
+  }
+}
+
+async function getUserById(nodeId: string): Promise<Record<string, any>> {
+  try {
+    requestCount++
+    const data = await (await fetch('https://api.github.com/graphql', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + process.env["gh-token"]
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        query: `{
+          node(id: "${nodeId}") {
+            ...on User {
+              id
+              login 
+              following(first: 100) {
+                nodes {
+                  id
+                  login
+                }
+                totalCount
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+              }
+            }
+            ...on Organization {
+              id
+              login
+            }
+          }
+        }`
+      })
+    })).json()
+    if(!data.data.node.following) {
+      return ({
+        'redis-updatedAt': Date.now(),
+        id: data.data.node.id,
+        login: data.data.node.login,
+        followings: [],
+        followingsCount: 0,
+      })
+    }
+    return ({
+      'redis-updatedAt': Date.now(),
+      id: data.data.node.id,
+      login: data.data.node.login,
+      followings: data.data.node.following.nodes.map(follwoing => follwoing.id),
+      followingsCount: data.data.node.following.totalCount,
+    })
+    // return data.data.user ? data.data.user.following.nodes.map(user => user.login) : []
+  } catch (e) {
+    if(e instanceof TypeError) {
+      console.error(e.message)
+    } else console.error(e)
+    return null
   }
 }
 
@@ -103,12 +285,9 @@ function findShortestUserPath(start: string, target: string, chain: string[]) {
   }
 }
 
-let found = false
-const users = [] as {user: string, followings: string[]}[]
 let circles: {parent: null | string, followings: string[]}[][] = []
 
-
-
+const fastestWayOnly = true
 async function circleSearch(
   target: string,
   start: string,
@@ -116,44 +295,52 @@ async function circleSearch(
   circle = 1
 ) {
   if(circle > 5) return
-  // const followingPromises: Promise<string[]>[] = []
+  console.log('\n\n---------- Starting circle', circle, '----------\n\n')
+  let found = false
   const followingPromises: {parent: string, followings: string[]}[] = []
+  let ignoredOnThisCircle = 0
   for(const login of logins) {
     for(const following of login.followings) {
+      t++
+      // console.log(t)
       const followingObject = { parent: following, followings: [] }
       followingPromises.push(followingObject)
-      if(following === target) {
-        console.log('Found !!!', circle)
-        found = true
-      }
-      if(searched.includes(following)) {
-        console.log('ignore', following)
-        followingObject.followings = users.find(user => user.user === following).followings
+
+      const redisUser = await client.json.get('users:' + following)
+      if(redisUser) {
+        if(redisUser.followings.includes(target)) found = true
+        // console.log('Ignoring:', redisUser.login)
+        ignoredOnThisCircle++
+        followingObject.followings = redisUser.followings
         continue
       }
-      searched.push(following)
-      // console.log(searched)
-      const followingPromise = await getFollowings(following)
-      users.push({ user: following, followings: followingPromise })
-      followingObject.followings = followingPromise
+      
+      const user = await getUserById(following)
+      if(!user) continue
+      console.log(requestCount, 'Saving new user', user.login)
+      client.json.set('users:' + user.id, '$', user)
+      
+      followingObject.followings = user.followings
       io.emit('following', {following: followingObject, circle: circle - 1})
-      console.log(`circle ${circle}:`,requestCount, following)
+      
+      if(user.followings.includes(target)) found = true
+      if(fastestWayOnly && found) break
     }
+    if(fastestWayOnly && found) break
   }
+  console.log('Total Loops:', t, 'Ignored on this circle:', ignoredOnThisCircle)
   const resolvedPromises = await Promise.all(followingPromises)
   const nextLogins = resolvedPromises.filter((v,i,a)=>a.findIndex(v2=>(v2.parent===v.parent))===i)
   circles.push(nextLogins)
-
   let concerneds = resolvedPromises.filter(x => x.followings.find(y => y === target))
   if(concerneds.length) {
     concerneds = resolvedPromises.filter(x => x.followings.find(y => y === target))
-    
+
     const chains: string[][] = []
     for(const concerned of concerneds) {
       let nextParent = concerned.parent
       const chain: string[] = [target]
       for(let i = circle - 1; i > 0; i--) {
-        // const concerned_ = circles[i].find(x => x.followings.find(y => y === nextParent))
         let concerned_
         if(i === circle - 1) {
           concerned_ = circles[i].find(x => x.parent === nextParent)
@@ -164,7 +351,7 @@ async function circleSearch(
       chain.push(start)
       chains.push(chain.reverse())
     }
-    console.log(chains)
+    console.log('chains', (await Promise.all(chains.map(chain => Promise.all(chain.map(userId => client.json.get('users:' + userId)))))).map(chain => chain.map(users => users.login)))
     return
   }
   await circleSearch(target, start, nextLogins, circle + 1)
@@ -189,14 +376,20 @@ let searched = []
 let chains: (string[])[] = []
 let requestCount = 0
 
+let t = 0
 app.post('/api/handshake', async (req, res) => {
+  requestCount = 0
   searched = []
   chains = []
   requestCount = 0
   circles = []
+  t = 0
   const {target, start}: {target: string, start: string} = req.body
-  await circleSearch(target, start)
-  console.log(circles)
+  const [targetUser, startUser] = await Promise.all([getUser(target), getUser(start)])
+  await client.json.set('users:' + targetUser.id, '$', targetUser)
+  await circleSearch(targetUser.id, startUser.id)
+  // console.log(circles)
+  console.log(requestCount, 'requests made')
   // console.log(circles)
   // console.log(users)
   // console.log(filterUselessDeviation(chains))
